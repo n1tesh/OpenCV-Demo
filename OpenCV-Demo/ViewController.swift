279e857142912645fad38fde6c0d807031a647f7
Nitesh Isave <nitesh.isave@infinxinc.com>
2017-09-19 11:51:33 +0530

    Initial Commit

---
+++ OpenCV-Demo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  OpenCV-Demo
+//
+//  Created by Nitesh Isave on 19/09/17.
+//  Copyright © 2017 Nitesh Isave. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
